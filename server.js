77ff011176b07766659278faeb4a2f3f1815e140
Bhargavjois <43719681+Bhargavjois@users.noreply.github.com>
2024-06-30 18:05:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 const { collection, query, orderBy, limit, startAfter } = require ('firebase-admin/firestore');
 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = {
+							"type": "service_account",
+							"project_id": process.env.PROJECT_ID,
+							"private_key_id": process.env.PRIVATE_KEY_ID,
+							"private_key": process.env.PRIVATE_KEY,
+							"client_email": process.env.CLIENT_MAIL,
+							"client_id": process.env.CLIENT_ID,
+							"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+							"token_uri": "https://oauth2.googleapis.com/token",
+							"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+							"client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+							"universe_domain": "googleapis.com"
+						} 	//require('./serviceAccountKey.json');
+
 const message_JSON = require('./error_codes.json');
 
 const messages = message_JSON.messages;
